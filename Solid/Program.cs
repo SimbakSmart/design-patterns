094a149fdb59f931cdff5028db3d1eea2e6c0802
Simbak Ramirez <si.ramirez@smart.corp>
2024-03-13 10:42:09 -0600

    003 Open-Closed Principle

--- Solid/Program.cs
+++ Solid/Program.cs
@@ -4,68 +4,241 @@ using static System.Console;
 namespace Solid
 {
 
-    public class Journal
+    public enum Color
     {
-        private readonly List<string> entries = new List<string>();
-
-        private static int count = 0;
-
-        public int AddEntry(string text)
-        {
-            entries.Add($"{++count}: {text}");
-            return count; // memento pattern!
-        }
-
-        public void RemoveEntry(int index)
-        {
-            entries.RemoveAt(index);
-        }
-
-        public override string ToString()
-        {
-            return string.Join(Environment.NewLine, entries);
-        }
-
-        // breaks single responsibility principle
-        //public void Save(string filename, bool overwrite = false)
-        //{
-        //    File.WriteAllText(filename, ToString());
-        //}
-
-        //public void Load(string filename)
-        //{
-
-        //}
-
-        //public void Load(Uri uri)
-        //{
-
-        //}
-
+        Red, Green, Blue
     }
 
-    // handles the responsibility of persisting objects
-    public class Persistence
+    public enum Size
     {
-        public void SaveToFile(Journal journal, string filename, bool overwrite = false)
+        Small, Medium, Large, Yuge
+    }
+
+    public enum Country
+    {
+        Mexico, Usas, Canada
+    }
+
+    public class Product
+    {
+        public string Name;
+        public Color Color;
+        public Size Size;
+        public Country Country;
+
+        public Product(string name, Color color, Size size)
         {
-            if (overwrite || !File.Exists(filename))
-                File.WriteAllText(filename, journal.ToString());
+            Name = name ?? throw new ArgumentNullException(paramName: nameof(name));
+            Color = color;
+            Size = size;
+        }
+
+        public Product(string name, Color color, Size size, Country country)
+        {
+            Name = name;
+            Color = color;
+            Size = size;
+            Country = country;
         }
     }
+
+    public class ProductFilter
+    {
+        // let's suppose we don't want ad-hoc queries on products
+        public IEnumerable<Product> FilterByColor(IEnumerable<Product> products, Color color)
+        {
+            foreach (var p in products)
+                if (p.Color == color)
+                    yield return p;
+        }
+
+        public static IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
+        {
+            foreach (var p in products)
+                if (p.Size == size)
+                    yield return p;
+        }
+
+        public static IEnumerable<Product> FilterBySizeAndColor(IEnumerable<Product> products, Size size, Color color)
+        {
+            foreach (var p in products)
+                if (p.Size == size && p.Color == color)
+                    yield return p;
+        } // state space explosion
+          // 3 criteria = 7 methods
+
+        // OCP = open for extension but closed for modification
+    }
+
+
+    // we introduce two new interfaces that are open for extension
+
+    public interface ISpecification<T>
+    {
+        bool IsSatisfied(Product p);
+    }
+    public interface IFilter<T>
+    {
+        IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> spec);
+    }
+
+    public class ColorSpecification : ISpecification<Product>
+    {
+        private Color color;
+
+        public ColorSpecification(Color color)
+        {
+            this.color = color;
+        }
+
+        public bool IsSatisfied(Product p)
+        {
+            return p.Color == color;
+        }
+    }
+
+
+
+    public class SizeSpecification : ISpecification<Product>
+    {
+        private Size size;
+
+        public SizeSpecification(Size size)
+        {
+            this.size = size;
+        }
+
+        public bool IsSatisfied(Product p)
+        {
+            return p.Size == size;
+        }
+    }
+
+    public class CountrySpecification : ISpecification<Product>
+    {
+        private Country country;
+
+        public CountrySpecification(Country country)
+        {
+            this.country = country;
+        }
+        public bool IsSatisfied(Product p)
+        {
+            return p.Country == country;
+        }
+    }
+
+    // combinator
+    public class AndSpecification<T> : ISpecification<T>
+    {
+        private ISpecification<T> first, second;
+
+        public AndSpecification(ISpecification<T> first, ISpecification<T> second)
+        {
+            this.first = first ?? throw new ArgumentNullException(paramName: nameof(first));
+            this.second = second ?? throw new ArgumentNullException(paramName: nameof(second));
+        }
+
+        public bool IsSatisfied(Product p)
+        {
+            return first.IsSatisfied(p) && second.IsSatisfied(p);
+        }
+    }
+    public class AndCountrySpecification<T> : ISpecification<T>
+    {
+        private ISpecification<T> first, second, third;
+        public AndCountrySpecification(ISpecification<T> first, ISpecification<T> second, ISpecification<T> third)
+        {
+            this.first = first ?? throw new ArgumentNullException(paramName: nameof(first));
+            this.second = second ?? throw new ArgumentNullException(paramName: nameof(second));
+            this.third = third ?? throw new ArgumentNullException(paramName: nameof(third));
+        }
+        public bool IsSatisfied(Product p)
+        {
+            return first.IsSatisfied(p) && second.IsSatisfied(p) && third.IsSatisfied(p);
+        }
+    }
+
+    public class BetterFilter : IFilter<Product>
+    {
+        public IEnumerable<Product> Filter(IEnumerable<Product> items, ISpecification<Product> spec)
+        {
+            foreach (var i in items)
+                if (spec.IsSatisfied(i))
+                    yield return i;
+        }
+    }
+
+
+
+
+
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            var j = new Journal();
-            j.AddEntry("I cried today.");
-            j.AddEntry("I ate a bug.");
-            WriteLine(j);
 
-            var p = new Persistence();
-            var filename = @"c:\temp\journal.txt";
-            p.SaveToFile(j, filename);
-            Process.Start(filename);
+            var apple = new Product("Apple", Color.Green, Size.Small);
+            var tree = new Product("Tree", Color.Green, Size.Large);
+            var house = new Product("House", Color.Blue, Size.Large);
+
+            Product[] products = { apple, tree, house };
+
+            var pf = new ProductFilter();
+            WriteLine("Green products (old):");
+            foreach (var p in pf.FilterByColor(products, Color.Green))
+                WriteLine($" - {p.Name} is green");
+
+            // ^^ BEFORE
+
+            // vv AFTER
+            var bf = new BetterFilter();
+            WriteLine("Green products (new):");
+            foreach (var p in bf.Filter(products, new ColorSpecification(Color.Green)))
+                WriteLine($" - {p.Name} is green");
+
+            WriteLine("Large products");
+            foreach (var p in bf.Filter(products, new SizeSpecification(Size.Large)))
+                WriteLine($" - {p.Name} is large");
+
+
+
+            WriteLine("Large blue items");
+            foreach (var p in bf.Filter(products,
+              new AndSpecification<Product>(new ColorSpecification(Color.Blue), new SizeSpecification(Size.Large)))
+            )
+            {
+                WriteLine($" - {p.Name} is big and blue");
+            }
+
+            var frist = new Product("Apple", Color.Green, Size.Small, Country.Mexico);
+            var second = new Product("Tree", Color.Green, Size.Large, Country.Canada);
+            var third = new Product("House", Color.Blue, Size.Large, Country.Canada);
+
+            Product[] list = { frist, second, third };
+
+            WriteLine("Large products *******");
+            foreach (var p in bf.Filter(list, new SizeSpecification(Size.Large)))
+                WriteLine($" - {p.Name} is large");
+
+
+            WriteLine("Country products *******");
+            foreach (var p in bf.Filter(list, new CountrySpecification(Country.Mexico)))
+                WriteLine($" - {p.Name} is Country {p.Country}");
+
+
+            WriteLine("Color Size And Country  items");
+            foreach (var p in bf.Filter(list,
+              new AndCountrySpecification<Product>(new ColorSpecification(Color.Blue), 
+              new SizeSpecification(Size.Large),new CountrySpecification(Country.Canada)))
+            )
+            {
+                WriteLine($" - {p.Name} Color {p.Color} Size {p.Size} Country {p.Country}");
+            }
+
         }
+
+
     }
 }
